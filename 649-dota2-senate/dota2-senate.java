7fe07f2cc84bc488099d01872fc76aa6b733873c
Siddharth77s <siddharthshinde9476@gmail.com>
2025-07-13 00:00:55 +0530

    Time: 12 ms (44.38%) | Memory: 45.9 MB (29.17%) - LeetSync

---
+++ 649-dota2-senate/dota2-senate.java
@@ -0,0 +1,31 @@
+class Solution {
+      public String predictPartyVictory(String senate) {
+            Queue<Integer> radiant = new LinkedList<>();
+            Queue<Integer> dire = new LinkedList<>();
+
+            // Populate the queues with indices of Radiant and Dire senators
+            for (int i = 0; i < senate.length(); i++) {
+                  if (senate.charAt(i) == 'R') {
+                        radiant.add(i);
+                  } else {
+                        dire.add(i);
+                  }
+            }
+
+            // Process the queues
+            while (!radiant.isEmpty() && !dire.isEmpty()) {
+                  int r = radiant.poll();
+                  int d = dire.poll();
+
+                  // The senator with the smaller index survives
+                  if (r < d) {
+                        radiant.add(r + senate.length());
+                  } else {
+                        dire.add(d + senate.length());
+                  }
+            }
+
+            // Return the winner
+            return radiant.isEmpty() ? "Dire" : "Radiant";
+      }
+}
